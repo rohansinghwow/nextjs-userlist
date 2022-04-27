e4cc5d08f8df135d1bfabfac2b7802e87f8d3b1a
rohansinghwow <rohansinghwow@gmail.com>
2022-04-27 11:35:17 +0530

    adding meta tags

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -8,10 +8,11 @@ export default function Layout({children , title}){
     return (
         <>
             <Head>
+                <title>{title}</title>
+
             <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-                <title>{title}</title>
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossOrigin="anonymous" />
             </Head>
             <Header/>
