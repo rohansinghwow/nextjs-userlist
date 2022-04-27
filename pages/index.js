1d7efcf5ea13b61ef793005142d0a389287cd300
rohansinghwow <rohansinghwow@gmail.com>
2022-04-27 18:55:09 +0530

    adding button margin and emojis

--- pages/index.js
+++ pages/index.js
@@ -8,9 +8,10 @@ export default function Home(){
             
             
             <h1>Mini Project using JSOn Placeholder with NextJS</h1>
-            <Link href={'/users'}><a className="btn btn-primary">Go to User List</a></Link>
+            <div className="p-4">
+            <Link href={'/users'}><a className="btn btn-primary ms-2 mb-2 ">🕺  User List 🕺</a></Link>
             <Link href={'/dogs'}><a className="btn btn-primary ms-2">🐕 Random Dogs 🐕</a></Link>
-            
+            </div>
         </Layout>
     )
 }
