2dfef94d53d12ed1b7b817b8021dad5cf72108b0
rohansinghwow <rohansinghwow@gmail.com>
2022-04-27 19:10:25 +0530

    resloving &apos error

--- pages/index.js
+++ pages/index.js
@@ -8,10 +8,10 @@ export default function Home(){
             
             
             <h1>Mini Project using JSOn Placeholder with NextJS</h1>
-            <div className="p-4">
-            <Link href={'/users'}><a className="btn btn-primary ms-2 mb-2 ">User List</a></Link>
-            <Link href={'/dogs'}><a className="btn btn-primary ms-2">Ah Yes , Doggo</a></Link>
-            </div>
+            
+            <Link href={'/users'}><a className="btn btn-primary ms-2 mb-2">User List</a></Link>
+            <Link href={'/dogs'}><a className="btn btn-primary ms-2">Ah Yes,Doggo</a></Link>
+            
         </Layout>
     )
 }
