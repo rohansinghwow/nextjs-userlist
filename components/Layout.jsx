d73c37a8e912f27cb717fc1c5f9d39fcfea6d8de
rohansinghwow <rohansinghwow@gmail.com>
2022-04-27 11:37:27 +0530

    renaming http-equiv to httpEquiv

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -12,7 +12,7 @@ export default function Layout({children , title}){
 
             <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossOrigin="anonymous" />
             </Head>
             <Header/>
