444ee135891e8d2053a9d40c99c2ab7653859838
rohansinghwow <rohansinghwow@gmail.com>
2022-04-27 11:40:57 +0530

    adding responsive table

--- pages/users/index.js
+++ pages/users/index.js
@@ -12,7 +12,7 @@ export default function Users({ user }) {
       
     <Layout title={"All Users"}>
       <h2>Number of Users :{user.length}</h2>
-
+    <div className="table-responsive">
       <table className="table table-hover">
         <thead>
           <tr>
@@ -55,6 +55,7 @@ export default function Users({ user }) {
           ))}
         </tbody>
       </table>
+      </div>
     </Layout>
    
   );
